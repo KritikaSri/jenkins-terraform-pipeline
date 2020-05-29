e60aec11f1083ffd39ca7ace5f5f6fb4998d330d
KritikaSri <51196851+KritikaSri@users.noreply.github.com>
2020-05-29 18:17:02 +0530

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -1,8 +1,8 @@
-provider "aws" {
+/*provider "aws" {
   region = "ap-south-1"
 }
 
 resource "aws_security_group" "jenkins-pipeline" {
   name        = "jenkins-pipeline"
   description = "built-by-jenkins-pipeline"
-}
+} */
